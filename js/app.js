c1dce8bbc08ad20ec1f84a2b6ee98ba8b4aefe5f
Ste-cracco <s.cracco@yahoo.it>
2022-07-22 16:30:43 +0200

    Problema Scritte al posto dei numeri

--- js/app.js
+++ js/app.js
@@ -3,23 +3,31 @@ const grigliaElement = document.querySelector('.griglia');
 
 for(let i=0; i<100; i++) {
     let numero = i+1;
+
+    const divElement = document.createElement('div');
+    divElement.append(numero);
+    divElement.classList.add('cella');
+
     
     if (numero % 3 === 0 && numero % 5 == 0) {
-        numero = 'FizzBuzz';     
+        divElement.append('FizzBuzz');
+        divElement.classList.add('multiplo_3_e_5');
     } 
-    else if (numero % 5 === 0) {
-        numero = 'buzz';   
+    else if (numero % 5 === 0) {  
+        divElement.append('Buzz');
+        divElement.classList.add('multiplo_5');
     }    
     else if (numero % 3 === 0) {
-        numero = 'fizz';
-    }  
-    console.log(numero)
+        divElement.append('Fizz');
+        divElement.classList.add('multiplo_3');
+    } 
 
-    // const divElement = document.createElement('div')
-    // divElement.append(number)
-    // divElement.classList.add('cella')
+    grigliaElement.append(divElement);   
 
-    // grigliaElement.append(divElement)
+    // Metodo "BackTick"
+    // grigliaElement.innerHTML += `<div class="cella"> ${numero} </div>`
 
-    grigliaElement.innerHTML += `<div class="cella"> ${numero} </div>`
+    // Metodo "Append"
+    
+     
 }
