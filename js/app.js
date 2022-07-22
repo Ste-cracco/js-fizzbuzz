a985774957538dcb4e3b705a71b70e3809129d2f
Ste-cracco <s.cracco@yahoo.it>
2022-07-22 17:09:46 +0200

    Esercizio completato

--- js/app.js
+++ js/app.js
@@ -3,31 +3,27 @@ const grigliaElement = document.querySelector('.griglia');
 
 for(let i=0; i<100; i++) {
     let numero = i+1;
-
-    const divElement = document.createElement('div');
-    divElement.append(numero);
+    const divElement = document.createElement('div');    
     divElement.classList.add('cella');
-
     
     if (numero % 3 === 0 && numero % 5 == 0) {
-        divElement.append('FizzBuzz');
+        numero = 'FizzBuzz';
         divElement.classList.add('multiplo_3_e_5');
     } 
     else if (numero % 5 === 0) {  
-        divElement.append('Buzz');
+        numero = 'Buzz';
         divElement.classList.add('multiplo_5');
     }    
     else if (numero % 3 === 0) {
-        divElement.append('Fizz');
+        numero = 'Fizz';
         divElement.classList.add('multiplo_3');
     } 
-
+    
+    divElement.append(numero);
     grigliaElement.append(divElement);   
 
     // Metodo "BackTick"
-    // grigliaElement.innerHTML += `<div class="cella"> ${numero} </div>`
+    // grigliaElement.innerHTML += `<div class="cella"> ${numero} </div>`   
 
-    // Metodo "Append"
-    
-     
 }
+
